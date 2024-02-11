7dd88f2fc21a3584f0f4e586ce5054529ab6bfd3
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-02-11 12:01:32 -0400

    Oke leak 🚀

--- apps/vanilla/src/main.ts
+++ apps/vanilla/src/main.ts
@@ -1,24 +1,40 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+// https://developer.chrome.com/docs/devtools/memory-problems#allocation-profile
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+// var x = [];
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// function grow() {
+//   console.log("growing", x.length);
+//   for (var i = 0; i < 10000; i++) {
+//     const newItem = document.createElement("div");
+//     newItem.textContent = "Hello World";
+//     document.body.appendChild(newItem);
+//   }
+//   x.push(new Array(1000000).join("x"));
+// }
+
+const button = document.getElementById("create") as HTMLButtonElement;
+
+function grow() {
+  // Add 100 event listeners to the button
+
+  for (var i = 0; i < 100; i++) {
+    button.addEventListener("click", () => console.log("clicked"));
+  }
+}
+
+button.addEventListener("click", grow);
+
+// var detachedTree;
+
+// function create() {
+//   console.log("creating");
+
+//   var ul = document.createElement("ul");
+//   for (var i = 0; i < 10; i++) {
+//     var li = document.createElement("li");
+//     ul.appendChild(li);
+//   }
+//   detachedTree = ul;
+// }
+
+// document.getElementById("create")!.addEventListener("click", create);
